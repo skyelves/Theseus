6582fbf1d6b78e243b24ceb17a5ca5ee3d888559
skyelves <ke.wang.kw754@yale.edu>
2021-11-15 19:06:53 -0800

    part2

---
+++ applications/hello_world/src/lib.rs
@@ -0,0 +1,12 @@
+#![no_std]
+// #![feature(plugin)]
+// #![plugin(application_main_fn)]
+
+// #[macro_use] extern crate log;
+#[macro_use] extern crate terminal_print;
+
+pub fn main() -> isize {
+    println!("Hello, world!");
+
+    0
+}
