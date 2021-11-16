d7571e14f956d1a5b7c00605ac6f09240d37f6e2
skyelves <ke.wang.kw754@yale.edu>
2021-11-16 11:15:59 -0800

    add comments

--- kernel/memuse/src/lib.rs
+++ kernel/memuse/src/lib.rs
@@ -20,13 +20,15 @@ pub enum MemType {
 pub type MemuseRes = Result<task::TaskRef, MemuseError>;
 
 pub fn mymemuse(tid: usize, mem_type: MemType) -> MemuseRes {
-    // info!("Hello, world! (from hello application)");"TaskNotExist"
+    // print to the kernel log
     info!("tid: {:?}, mem_type: {:?}", tid, mem_type);
+    // check if the memory type is valid
     match mem_type {
         MemType::Heap => (),
         MemType::CallStack => (),
         _ => return Err(MemuseError::InvalidMemtype),
     }
+    // check if the task exists
     let task_ref = task::get_task(tid).ok_or(MemuseError::TaskNotExist);
     
     task_ref
